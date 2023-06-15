20cb046a2054f5da253f007478a666ecbd670b7b
Raphael Fiamoncini <85579710+BrunoSchmitz4@users.noreply.github.com>
2023-06-15 14:12:32 -0300

    Implementacao de Classe

--- TerceiroAplicativo/Program.cs
+++ TerceiroAplicativo/Program.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Globalization;
+using TerceiroAplicativo;
 
 namespace TerceiroProjeto
 {
     class Program
     {
+        // C# - POO: Classes, atributos, métodos e membros estáticos
         static void Main(string[] args)
         {
-            // C# - POO: Classes, atributos, métodos e membros estáticos
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
 
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Entre com as medidas do triângulo y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            CalcTrian();
-            
+            double p = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A maior área entre o triângulo X ({0}) e o triângulo ({1}) é a do triângulo X",areaX, areaY);
+            }
+            else
+            {
+                Console.WriteLine("A maior área entre o triângulo X ({0}) e o triângulo ({1}) é a do triângulo Y", areaX, areaY);
+            }
         }
 
         // Função que faz o Cálculo dos Triângulos sem uso de POO (Versão Tutor)
---
+++ TerceiroAplicativo/Triangulo.cs
@@ -0,0 +1,11 @@
+﻿namespace TerceiroAplicativo
+{
+    // A classe pode ser considerada um tipo personalizado
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
