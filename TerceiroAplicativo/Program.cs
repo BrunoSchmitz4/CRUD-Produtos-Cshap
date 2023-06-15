5fda22cd579a55cb5e6d5ed9060362161555993c
Raphael Fiamoncini <85579710+BrunoSchmitz4@users.noreply.github.com>
2023-06-15 13:35:26 -0300

    Atualizacao

--- TerceiroAplicativo/Program.cs
+++ TerceiroAplicativo/Program.cs
@@ -9,6 +9,46 @@ namespace TerceiroProjeto
         {
             // C# - POO: Classes, atributos, métodos e membros estáticos
 
+
+
+            CalcTrian();
+            
+        }
+
+        // Função que faz o Cálculo dos Triângulos sem uso de POO (Versão Tutor)
+        static void CalcTrianTutor()
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double px = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(px * (px - xA) * (px - xB) * (px - xC));
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double py = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(py * (py - yA) * (py - yB) * (py - yC));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+        // Função que faz o Cálculo dos Triângulos sem uso de POO (Versão própria)
+        static void CalcTrian()
+        {
             Console.WriteLine("Insira o valor de A do triângulo X: ");
             double ax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Insira o valor de B do triângulo X: ");
@@ -30,16 +70,17 @@ namespace TerceiroProjeto
             double cy = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             double py = (ay + by + cy) / 2;
-            double raizy = py * (py - ay) * (py - bx) * (py - cx);
+            double raizy = py * (py - ay) * (py - by) * (py - cy);
             double areay = Math.Sqrt(raizy);
             double trianguloY = areay;
             Console.WriteLine("A área do triângulo X é: " + trianguloX);
             Console.WriteLine("A área do triângulo Y é: " + trianguloY);
 
-            if(trianguloY > trianguloX)
+            if (trianguloY > trianguloX)
             {
                 Console.WriteLine("Logo, a área do triângulo Y é maior que a do triângulo X");
-            }else if (trianguloY == trianguloX)
+            }
+            else if (trianguloY == trianguloX)
             {
                 Console.WriteLine("Logo, a área do triângulo Y é igual a do triângulo X");
             }
@@ -48,5 +89,6 @@ namespace TerceiroProjeto
                 Console.WriteLine("Logo, a área do triângulo X é igual a do triângulo Y");
             }
         }
+
     }
 }
